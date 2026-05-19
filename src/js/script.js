2c40a31e5375c55c7c24d0a4c25a085a93c82bd2
Wellington <welltecnic@hotmail.com>
2026-05-19 09:37:22 -0300

    feat: funcionalidades do menu hamburguer

--- src/js/script.js
+++ src/js/script.js
@@ -16,3 +16,16 @@ window.onload = function(){
 function trocar(cor){
  document.body.style.background=cor;
 }
+
+//  MENU HAMBURGUER
+
+document.addEventListener("DOMContentLoaded",()=>{
+    const menuIcone = document.getElementById("menu-icone");
+    const navMenu =document.querySelector("nav");
+    if(menuIcone && navMenu){
+        menuIcone.onclick=()=>{
+                navMenu.classList.toggle("active");
+                menuIcone.classList.toggle("open");
+        }
+    }
+})
